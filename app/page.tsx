2f21ea3e1635545ace96f9715d72204e6f264710
alexb017 <alexbacirea@gmail.com>
2023-08-04 23:33:55 +0300

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import Carousel from "@/components/carousel";
+import Footer from "@/components/footer";
+
 export default function Home() {
   return (
     <div>
       <h1>Welcome!</h1>
+      <Carousel />
+      <Footer />
     </div>
   );
 }
