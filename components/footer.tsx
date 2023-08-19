0a81aef8f29953ed81e95578fd967468a3dbd4c4
alexb017 <alexbacirea@gmail.com>
2023-08-19 14:30:41 +0300

    minor chnages

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,3 +1,7 @@
 export default function Footer() {
-  return <p className="text-xs">&copy; 2023, Urban Store</p>;
+  return (
+    <div className="mx-auto w-full max-w-7xl py-8">
+      <p className="text-xs">&copy; 2023, Urban Store.</p>
+    </div>
+  );
 }
